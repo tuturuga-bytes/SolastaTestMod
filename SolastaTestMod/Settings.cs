ab5fd0426e3ae8205e27db435f1600602c42c63d
tuturuga-bytes <tuturuga@tutamail>
2021-07-16 23:44:08 +0700

    initial commit by SolastaMod

---
+++ SolastaTestMod/Settings.cs
@@ -0,0 +1,17 @@
+using UnityModManagerNet;
+
+namespace SolastaTestMod
+{
+    public class Core
+    {
+
+    }
+
+    public class Settings : UnityModManager.ModSettings
+    {
+        public bool toggleTest1 = false;
+        public bool toggleTest2 = true;
+        public bool toggleTest3 = false;
+        public bool toggleTest4 = true;
+    }
+}
